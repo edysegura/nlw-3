1ff49d599fe020c7a24ebff71df269fa2368ebdc
Edy Segura <edysegura@gmail.com>
2020-10-14 21:22:45 -0300

    Add property to component title

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 
-function Title() {
+interface TitleProps {
+  text: string;
+}
+
+function Title(props: TitleProps) {
   return (
-    <h1>Hello NLW3</h1>
-  )
+    <h1>{ props.text }</h1>
+  );
 }
 
 function App() {
   return (
     <div className="App">
-      <Title />
+      <Title text="Title 1" />
+      <Title text="Title 2" />
+      <Title text="Title 3" />
+      <Title text="Title 4" />
     </div>
   );
 }
