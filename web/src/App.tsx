1934f7e9bb4af32350f64d2cb7c5a39fa4d2f14d
Edy Segura <edysegura@gmail.com>
2020-10-15 21:42:44 -0300

    Add CSS for landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
 
 import './styles/global.css';
-
-interface TitleProps {
-  text: string;
-}
-
-function Title(props: TitleProps) {
-  return (
-    <h1>{ props.text }</h1>
-  );
-}
+import './styles/pages/landing.css';
 
 function App() {
   return (
-    <div className="App">
-      <Title text="Title 1" />
-      <Title text="Title 2" />
-      <Title text="Title 3" />
-      <Title text="Title 4" />
+    <div id="landing-page">
+      <h1>Happy</h1>
     </div>
   );
 }
