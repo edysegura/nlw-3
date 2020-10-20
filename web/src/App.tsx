d53b879363605e3d63a5201e7639365c5f1d6a57
Edy Segura <edysegura@gmail.com>
2020-10-19 22:05:26 -0300

    Add elements to landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,10 +3,19 @@ import React from 'react';
 import './styles/global.css';
 import './styles/pages/landing.css';
 
+import logoImage from './images/logo.svg';
+
 function App() {
   return (
     <div id="landing-page">
-      <h1>Happy</h1>
+      <div className="content-wrapper">
+        <img src={logoImage} alt="Happy" />
+
+        <main>
+          <h1>Bring the happiness to the world</h1>
+          <p>Pay a visit to the orphanage and change the day of children</p>
+        </main>
+      </div>
     </div>
   );
 }
