50e354e0b8effa561ad09a80250ea16f1a661f90
Edy Segura <edysegura@gmail.com>
2020-10-21 21:50:34 -0300

    Enhance button with a icon

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
 
 import './styles/global.css';
 import './styles/pages/landing.css';
@@ -21,7 +22,9 @@ function App() {
           <span>Minas Gerais</span>
         </div>
 
-        <a href="#home" className="enter-app"> Enter </a>
+        <a href="#home" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </a>
       </div>
     </div>
   );
