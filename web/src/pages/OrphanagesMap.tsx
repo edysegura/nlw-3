225e5729d05d5f7d4830344907b31bef3cd20e6d
Edy Segura <edysegura@gmail.com>
2020-11-09 07:48:09 -0300

    Implement HTML for OrphanagesMap

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { FiPlus } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import mapMarkerImg from '../images/logo.svg';
 
 function OrphanagesMap() {
   return (
-    <h1>Hi there!</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+        <footer>
+          <strong>Santa Rita do Sapucaí</strong>
+          <span>Minas Gerais</span>
+        </footer>
+      </aside>
+
+      <div></div>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
   );
 }
 
